b835503dfccb5de5be6b66d17fd4d3e6d1756e95
mousa <jjkppk@163.com>
2018-07-31 23:59:23 +0800

    webpack

---
+++ webpack/webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+          {
+            test: /\.css$/,
+             use: [
+               'style-loader',
+               'css-loader'
+             ]
+           },
+           {
+            test: /\.js$/,
+            exclude: /(node_modules|bower_components)/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['env']
+              }
+            }
+          },
+          {
+            test: /\.scss$/,
+            use: [
+                "style-loader", // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS
+            ]
+          }
+      ]
+   }
+};
